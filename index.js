4c258a186415e328ca72155a25ef5ff0c721cf00
Amer Mall <amer@icloud.com>
2022-03-01 21:46:16 -0500

    Submit assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// const input = document.getElementById('input');
+// input.addEventListener("click", clickAlert);
+
+// const main = document.getElementById('main');
+// // console.log(main);
+// main.addEventListener("dblclick", clickAlert);
+
+// function clickAlert() {
+//   return alert(`Hee hee, that tickles!`);
+// }
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
